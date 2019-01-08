605b12ab378f17bca7627842472ec69abdb75b6d
diepz <shock88h@gmail.com>
2019-01-08 14:27:30 +0700

    page admin

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -10,7 +10,9 @@
 </nav>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Xem blog</a>
+
     <button type="button" class="btn btn-secondary">Baì đăng mới</button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -24,4 +26,4 @@
     <li><a href="#news"></a></li>
     <li><a href="#contact"></a></li>
     <li><a href="#about"></a></li>
-<
+</ul>
