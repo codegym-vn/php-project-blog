d9a3d861aa581559814a0204c98f9f1104069141
DungTV <anhdunngcoi@gmail.com>
2019-01-08 15:19:15 +0700

    view list blog user

--- resources/views/user/blog.blade.php
+++ resources/views/user/blog.blade.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dung
- * Date: 08/01/2019
- * Time: 11:41
- */
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Bootstrap Boilerplate... -->
+
+    <div class="panel-body">
+        <!-- Display Validation Errors -->
+
+        <form action="{{ url('user.blog') }}" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="blog" class="col-sm-3 control-label">Blog</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Blog
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+@endsection
