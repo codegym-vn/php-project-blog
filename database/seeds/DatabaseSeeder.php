8316383e2c853b77b286fcaffaf1c2a1654939be
DungTV <anhdunngcoi@gmail.com>
2019-01-10 14:47:02 +0700

    api

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call([PostsTableSeeder::class,
-             UserTableSeeder::class]);
+             UserTableSeeder::class,
+             ArticlesTableSeeder::class]);
     }
 }
 
