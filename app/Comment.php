5a076baee5d3ee9a50d473efeeb99e2889296880
DungTV <anhdunngcoi@gmail.com>
2019-01-21 15:13:57 +0700

    nav

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $table = "comments";
+
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
 }
