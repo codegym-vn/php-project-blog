710ff4dd10774be818c03571fc10b4de08a734bb
diepz <shock88h@gmail.com>
2019-01-21 17:38:29 +0700

    nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,122 +27,38 @@
 </head>
 <body>
 <div class="container">
-    <div class="col-12-md">
-        <nav class="level">
-            <p class="level-item has-text-centered">
-                <a class="link is-info" href="http://localhost:4200/post">Home</a>
-            </p>
-            <p class="level-item has-text-centered">
-                <a class="link is-info" href="#">Menu</a>
-            </p>
-            <p class="level-item has-text-centered" style="font-size: 25px">
-                <a href="{{route('home')}}">BLOG</a>
-            </p>
-            <p class="level-item has-text-centered">
-                <a class="link is-info" href="{{ route('admin.post.list') }}">My BLog</a>
-            </p>
-            <p class="level-item has-text-centered">
-                <a class="link is-info" href="#">Contact</a>
-            </p>
-        </nav>
-        <nav class="navbar navbar-expand-lg navbar-light flex-column flex-md-row bd-navbar sticky-top">
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0" action="{{route('admin.post.search')}}" method="get">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
-                           name="keyword">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    @guest
+    <div class="col-md-12">
+        <div class="sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="{{route('home')}}">BLOG</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="http://localhost:4200/post">Home <span class="sr-only">(current)</span></a>
+                        </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link" href="https://codegymvn.slack.com/messages/DDZMMHF1V/">Content</a>
                         </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                        <li class="nav-item ">
+                            <a class="nav-link " href="{{ route('admin.post.list') }}" >
+                                Menu
                             </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-
-                                <a class="dropdown-item" href="{{route('auth.passwords.changePassword')}}">
-                                    Change password
-                                </a>
-
-                                <a class="dropdown-item" href="{{route('auth.profile.edit')}}">
-                                    Profile
-                                </a>
-
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
                         </li>
-                    @endguest
-                </ul>
-            </div>
-        </nav>
-        <nav class="level">
-            <div class="level-item has-text-centered">
-                <div>
-                    <p class="heading">Tweets</p>
-                    <p class="title">3,456</p>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
                 </div>
-            </div>
-            <div class="level-item has-text-centered">
-                <div>
-                    <p class="heading">Following</p>
-                    <p class="title">123</p>
-                </div>
-            </div>
-            <div class="level-item has-text-centered">
-                <div>
-                    <p class="heading">Followers</p>
-                    <p class="title">456K</p>
-                </div>
-            </div>
-            <div class="level-item has-text-centered">
-                <div>
-                    <p class="heading">Likes</p>
-                    <p class="title">789</p>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
+            </nav>
+        </div>
             @yield('content')
-        </main>
     </div>
 </div>
 </div>
