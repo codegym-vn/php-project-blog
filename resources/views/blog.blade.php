68e17bff79ea63bb31597bbdec754321e255a322
trinh04042000 <letrinh04042000@gmail.com>
2019-01-31 17:20:25 +0700

    Tester

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -72,38 +72,6 @@
                                         of the card's content.</p>
                                 </div>
                             </div>
-                            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-                                <div class="card-header">Header</div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Dark card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                        of the card's content.</p>
-                                </div>
-                            </div>
-                            <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
-                                <div class="card-header">Header</div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Warning card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                        of the card's content.</p>
-                                </div>
-                            </div>
-                            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-                                <div class="card-header">Header</div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Info card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                        of the card's content.</p>
-                                </div>
-                            </div>
-                            <div class="card bg-light mb-3" style="max-width: 18rem;">
-                                <div class="card-header">Header</div>
-                                <div class="card-body">
-                                    <h5 class="card-title">Light card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                        of the card's content.</p>
-                                </div>
-                            </div>
                         </div>
                         @endforeach
                 </div>
